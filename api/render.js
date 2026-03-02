0b86f2a505524c365ae626a229750e758a15d059
HA <burhon.2211.tj@gmail.com>
2026-03-02 23:28:19 +0500

    add index.html as main page

--- api/render.js
+++ api/render.js
@@ -1,81 +1,65 @@
 export function render(data) {
-  const cardBox = document.querySelector(".cardBox");
+ const cardBox = document.querySelector('.cardBox')
 
-  const sreeCard = location.href.includes("home.html")
-    ? data.slice(0, 3)
-    : data;
-  if (!cardBox)  return;
-  
+ const sreeCard = location.href.includes('index.html') ? data.slice(0, 3) : data
+ if (!cardBox) return
 
-  cardBox.innerHTML = "";
+ cardBox.innerHTML = ''
 
-  sreeCard.forEach((element) => {
-  
+ sreeCard.forEach((element) => {
+  const card = document.createElement('div')
 
-    const card = document.createElement("div");
-  
+  const cardImage = document.createElement('div')
 
+  const cardActions = document.createElement('div')
 
+  cardActions.className = 'card__actions'
+  cardImage.className = 'card__image'
+  card.className = 'card'
 
-    const cardImage = document.createElement("div");
+  const btnInfo = document.createElement('button')
+  btnInfo.textContent = 'Info'
+  btnInfo.onclick = () => {
+   localStorage.setItem('id', element.id)
+   window.location.href = './productid.html'
+  }
 
+  const btnAdd = document.createElement('button')
+  btnAdd.textContent = 'Add'
+  btnAdd.onclick = () => addToCart(element)
 
-    const cardActions = document.createElement("div");
+  const img = document.createElement('img')
+  img.src = element.fields.image[0].url
+  img.alt = element.fields.name
 
-    
-    cardActions.className = "card__actions";
-        cardImage.className = "card__image";
-      card.className = "card";
-  
-    const btnInfo = document.createElement("button");
-    btnInfo.textContent = "Info";
-    btnInfo.onclick = () => {
-      localStorage.setItem("id", element.id);
-      window.location.href = "./productid.html";
-    };
+  const cardContent = document.createElement('div')
+  cardContent.className = 'card__content'
 
-    const btnAdd = document.createElement("button");
-    btnAdd.textContent = "Add";
-    btnAdd.onclick = () => addToCart(element);
+  const title = document.createElement('h3')
+  title.textContent = element.fields.name
 
+  const price = document.createElement('p')
+  price.textContent = `$${element.fields.price}`
 
-    const img = document.createElement("img");
-    img.src = element.fields.image[0].url;
-    img.alt = element.fields.name;
+  cardActions.append(btnInfo, btnAdd)
+  cardImage.append(cardActions, img)
+  cardContent.append(title, price)
 
-  
-    const cardContent = document.createElement("div");
-    cardContent.className = "card__content";
-
-    const title = document.createElement("h3");
-    title.textContent = element.fields.name;
-
-    const price = document.createElement("p");
-    price.textContent = `$${element.fields.price}`;
-
-
-    cardActions.append(btnInfo, btnAdd);
-    cardImage.append(cardActions, img);
-    cardContent.append(title, price);
-
-    card.append(cardImage, cardContent);
-    cardBox.append(card);
-  });
+  card.append(cardImage, cardContent)
+  cardBox.append(card)
+ })
 }
 
 function addToCart(product) {
+ let productsToCarts = JSON.parse(localStorage.getItem('productsToCarts')) || []
 
-  let productsToCarts = JSON.parse(localStorage.getItem("productsToCarts")) || [];
+ const item = productsToCarts.find((i) => i.id === product.id)
 
+ if (item) {
+  item.quantity++
+ } else {
+  productsToCarts.push({ ...product, quantity: 1 })
+ }
 
-  const item = productsToCarts.find((i) => i.id === product.id);
-
-  if (item) {
-    item.quantity++; 
-  } else {
-    productsToCarts.push({ ...product, quantity: 1 });
-  }
-
-  localStorage.setItem("productsToCarts", JSON.stringify(productsToCarts));
-
+ localStorage.setItem('productsToCarts', JSON.stringify(productsToCarts))
 }
